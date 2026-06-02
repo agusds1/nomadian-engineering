b75fd089cb095e5b9178eb1a7037e689ea7658c6
agusds1 <25agustusan@gmail.com>
2026-06-02 13:39:41 +0700

    update ui

--- src/components/FloatingContact.jsx
+++ src/components/FloatingContact.jsx
@@ -1,4 +1,6 @@
 import ReactGA from "react-ga4";
+import { MessageCircle, X } from "lucide-react";
+
 import {
   FaWhatsapp,
   FaInstagram
@@ -20,18 +22,80 @@ function FloatingContact({ open, setOpen }) {
         <div className="absolute bottom-20 right-0 flex flex-col items-center gap-3">
 
         {/* WhatsApp */}
-        <a
-        href="https://wa.me/6281213129318"
-        onClick={() => {
+        <div className="relative group">
+
+          <span
+            className="
+              absolute right-16 top-1/2
+              -translate-y-1/2
+              bg-gray-900 text-white
+              text-xs px-3 py-1 rounded-lg
+              opacity-0 group-hover:opacity-100
+              transition-all duration-300
+              whitespace-nowrap
+              pointer-events-none
+            "
+          >
+          Chat WhatsApp
+          </span>
+          <a
+            href="https://wa.me/6281213129318"
+            onClick={() => {
+              ReactGA.event({
+                category: "Contact",
+                action: "Click WhatsApp",
+                label: "Floating Contact",
+                });
+              }}
+              target="_blank"
+              rel="noreferrer"
+              className="
+              w-14 h-14
+              bg-white
+              rounded-full
+              shadow-xl
+              flex items-center justify-center
+              hover:scale-110
+              transition-all duration-300
+              "
+              >
+            <FaWhatsapp
+              size={30}
+              className="text-green-500"
+            />
+          </a>
+
+        </div>
+
+        {/* Email */}
+        <div className="relative group">
+          <span
+            className="
+              absolute right-16 top-1/2
+              -translate-y-1/2
+              bg-gray-900 text-white
+              text-xs px-3 py-1 rounded-lg
+              opacity-0 group-hover:opacity-100
+              transition-all duration-300
+              whitespace-nowrap
+              pointer-events-none
+            "
+          >
+          Kirim Email
+          </span>
+          
+          <a
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=arcadian.lpk@gmail.com&su=Informasi%20Bootcamp%20Nomadian"
+          onClick={() => {
           ReactGA.event({
             category: "Contact",
-            action: "Click WhatsApp",
+            action: "Click Email",
             label: "Floating Contact",
           });
-        }}
-        target="_blank"
-        rel="noreferrer"
-        className="
+         }}
+          target="_blank"
+          rel="noreferrer"
+          className="
           w-14 h-14
           bg-white
           rounded-full
@@ -39,44 +103,33 @@ function FloatingContact({ open, setOpen }) {
           flex items-center justify-center
           hover:scale-110
           transition-all duration-300
-        "
-       >
-        <FaWhatsapp
-        size={30}
-        className="text-green-500"
-        />
-       </a>
+          "
+          >
+          <MdEmail
+          size={30}
+          className="text-red-500"
+          />
+          </a>
+        </div>
 
-        {/* Email */}
-        <a
-        href="https://mail.google.com/mail/?view=cm&fs=1&to=arcadian.lpk@gmail.com&su=Informasi%20Bootcamp%20Nomadian"
-        onClick={() => {
-        ReactGA.event({
-          category: "Contact",
-          action: "Click Email",
-          label: "Floating Contact",
-        });
-        }}
-        target="_blank"
-        rel="noreferrer"
-        className="
-        w-14 h-14
-        bg-white
-        rounded-full
-        shadow-xl
-        flex items-center justify-center
-        hover:scale-110
-        transition-all duration-300
-        "
-        >
-        <MdEmail
-        size={30}
-        className="text-red-500"
-       />
-       </a>
+        {/* Instagram */}
+        <div className="relative group">
+          <span
+            className="
+            absolute right-16 top-1/2
+            -translate-y-1/2
+            bg-gray-900 text-white
+            text-xs px-3 py-1 rounded-lg
+            opacity-0 group-hover:opacity-100
+            transition-all duration-300
+            whitespace-nowrap
+            pointer-events-none
+            "
+          >
+         Instagram
+         </span>
 
-         {/* Instagram */}
-        <a
+          <a
           href="https://instagram.com/nomadian.engineering"
           onClick={() => {
             ReactGA.event({
@@ -96,7 +149,7 @@ function FloatingContact({ open, setOpen }) {
             hover:scale-110
             transition-all duration-300
           "
-         >
+          >
          <FaInstagram
          size={28}
           className="text-pink-500"
@@ -104,15 +157,19 @@ function FloatingContact({ open, setOpen }) {
           </a>
 
         </div>
+      </div>
 
       )}
 
       <button
         onClick={() => setOpen(!open)}
-        className="w-16 h-16 rounded-full bg-green-500 text-white text-2xl shadow-2xl hover:scale-110 transition-all duration-300"
+        className="w-16 h-16 rounded-full bg-blue-900 text-white text-2xl shadow-2xl flex items-center justify-center hover:scale-110 transition-all duration-300"
       >
-        {open ? "✕" : "💬"}
-      </button>
+        {open ? (  <X size={28} />
+        ) : (
+  <MessageCircle size={26} />
+        )}
+        </button>
 
     </div>
 
